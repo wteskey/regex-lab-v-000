9bf22a68e887956954fe4f589c70cdf65a27be3a
William J Teskey <willjteskey@gmail.com>
2018-04-13 23:29:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/^[aeiouAEIOU]\w+/)
+    true
+  elsif word.match(/^[^aeiouAEIOU]\w+/)
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
