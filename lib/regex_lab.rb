19378156af11c7f990079920b7190e0456c26cb5
William J Teskey <willjteskey@gmail.com>
2018-04-13 23:29:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/^[A-Z].+\./)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
